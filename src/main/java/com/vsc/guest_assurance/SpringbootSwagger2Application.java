7437d175694ff9769b5da955df9f83313305d393
kmj121 <921301172@qq.com>
2021-01-19 17:37:03 +0800

    点赞

--- src/main/java/com/vsc/guest_assurance/SpringbootSwagger2Application.java
+++ src/main/java/com/vsc/guest_assurance/SpringbootSwagger2Application.java
@@ -1,11 +1,15 @@
 package com.vsc.guest_assurance;
 
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -13,6 +17,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableTransactionManagement
 @MapperScan("com.vsc.guest_assurance.dao")
 public class SpringbootSwagger2Application {
+
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        //配置sqlserver数据库的方言
+        properties.setProperty("dialect","sqlserver");
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSwagger2Application.class, args);
     }
